454f1c7e722e91b87f0adb50261b29e9ed0c4ddd
tamastyukodi <tamas.tyukodi@rindus.de>
2025-03-06 19:04:00 +0100

    fix refresh time

--- src/main/java/com/myproject/video/video_platform/service/security/AuthService.java
+++ src/main/java/com/myproject/video/video_platform/service/security/AuthService.java
@@ -169,7 +169,7 @@ public class AuthService {
         jwtCookie.setHttpOnly(true);
         jwtCookie.setSecure(true);
         jwtCookie.setPath("/");
-        jwtCookie.setMaxAge(3600);
+        jwtCookie.setMaxAge(30);
         jwtCookie.setDomain(appDomain);
         jwtCookie.setAttribute("SameSite", "None");
         response.addCookie(jwtCookie);
@@ -188,7 +188,7 @@ public class AuthService {
         csrfCookie.setHttpOnly(false);
         csrfCookie.setSecure(true);
         csrfCookie.setPath("/");
-        csrfCookie.setMaxAge(3600);
+        csrfCookie.setMaxAge(30);
         response.addCookie(csrfCookie);
     }
 
