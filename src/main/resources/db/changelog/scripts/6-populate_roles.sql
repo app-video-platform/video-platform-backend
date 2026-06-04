a5fc670ea306e0f12478d7953a06a4ee3f252744
tamastyukodi <tamas.tyukodi@rindus.de>
2026-06-04 16:13:13 +0200

    fix liquidbase scripts

--- src/main/resources/db/changelog/scripts/6-populate_roles.sql
+++ src/main/resources/db/changelog/scripts/6-populate_roles.sql
@@ -1,7 +1,2 @@
-INSERT INTO roles (role_name)
-SELECT 'ADMIN'
-WHERE NOT EXISTS (SELECT 1 FROM roles WHERE role_name = 'ADMIN' OR role_name = 'admin');
-
-INSERT INTO roles (role_name)
-SELECT 'USER'
-WHERE NOT EXISTS (SELECT 1 FROM roles WHERE role_name = 'USER' OR role_name = 'user');
+INSERT INTO roles (role_name) VALUES ('admin');
+INSERT INTO roles (role_name) VALUES ('user');
